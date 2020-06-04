95053f1998c5a7d2d1120ab90bacef0b9b5c9799
Anton Fedoruk <antobn.fedoruk@gmail.com>
2020-06-04 21:55:52 +0300

    Modifying of message entity

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -3,31 +3,31 @@ package com.example.sweater.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 //Hibernate automatically translates the entity into a table.
 @Getter
 @Setter
 @Entity
 public class Message {
-    public Message() {
-    }
+    public Message() {}
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        author = user;
     }
-
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Integer id;
-
     private String text;
     private String tag;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 
-
+    public String getAuthorName(){
+        return author != null ? author.getUsername() : "<none>";
+    }
 }
