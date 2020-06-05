eadb3c1dc918938f546084340f854842a1786c63
Anton Fedoruk <antobn.fedoruk@gmail.com>
2020-06-05 11:55:55 +0300

    Added findByTag method to MessageRepository. Created User Repository

--- src/main/java/com/example/sweater/repository/MessageRepository.java
+++ src/main/java/com/example/sweater/repository/MessageRepository.java
@@ -3,9 +3,12 @@ package com.example.sweater.repository;
 import com.example.sweater.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 //the repository holds message records
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 public interface MessageRepository extends CrudRepository<Message, Integer> {
+    List<Message> findByTag(String tag);
 }
