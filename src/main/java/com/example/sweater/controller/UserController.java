5cda3c05afeac019ebe23101cad0855d445bda37
Anton Fedoruk <antobn.fedoruk@gmail.com>
2020-06-15 11:58:31 +0300

    Access rights by role

--- src/main/java/com/example/sweater/controller/UserController.java
+++ src/main/java/com/example/sweater/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.sweater.domain.Role;
 import com.example.sweater.domain.User;
 import com.example.sweater.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user") //global mapping to all following methods
+@PreAuthorize("hasAuthority('ADMIN')") //user must has ADMIN authority to get access to the nested methods
 public class UserController {
     @Autowired
     private UserRepository userRepository;
